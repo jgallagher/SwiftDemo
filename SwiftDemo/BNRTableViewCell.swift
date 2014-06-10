da9dce178e401a4623c53d8efe0ed0852b67b1b7
John Gallagher <jgallagher@bignerdranch.com>
2014-06-10 07:53:04 -0400

    Remove dead code in BNRTableViewCell

--- SwiftDemo/BNRTableViewCell.swift
+++ SwiftDemo/BNRTableViewCell.swift
@@ -13,15 +13,4 @@ class BNRTableViewCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel
     @IBOutlet var ageLabel: UILabel
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
